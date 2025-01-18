df821485c3d9ca15535c5f4b49edf4edd4f5fbe0
Анатолий <homenkotolya2@gmail.com>
2025-01-18 23:31:25 +0700

    2.Реализовал подсчет частоты слов в тексте. Программа считывает строку текста,с помощью регулярного выражения @"\W+" разбиваем строку на слова, игнорируя все знаки препинания и пробелы. Регулярное выражение \W+ означает «один или несколько символов, которые не являются буквами или цифрами». Приводим текст к нижнему регистру с помощью ToLower() что бы слова с заглвной и с маленькой считались как одно и тоже .Для подсчёта частоты слов используется Dictionary<string, int>, где ключ — это слово, а значение — количество его вхождений. Если слово уже есть в словаре, увеличиваем его частоту. Если нет — добавляем слово в словарь с начальным значением 1. Разделил на библиотеку и консольное приложение, где библиотека предоставляет логику, а консольное приложение является пользовательским интерфейсом.

---
+++ Algorithms/Countingfrequency.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Algorithms
+    {
+        public class Countingfrequency
+        {
+            public static Dictionary<string, int> CountWordFrequency(string text)
+            {
+                var wordCount = new Dictionary<string, int>();
+
+                string[] words = Regex.Split(text.ToLower(), @"\W+");
+
+                foreach (var word in words)
+                {
+                    if (!string.IsNullOrEmpty(word))
+                    {
+                        if (wordCount.ContainsKey(word))
+                        {
+                            wordCount[word]++;
+                        }
+                        else
+                        {
+                            wordCount[word] = 1;
+                        }
+                    }
+                }
+
+                return wordCount;
+            }
+        }
+    }
+
+
+
+
---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using Algorithms;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст:");
+            string input = Console.ReadLine();
+         
+            var wordCount = Countingfrequency.CountWordFrequency(input);
+
+            Console.WriteLine("Частота слов:");
+            foreach (var entry in wordCount)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+    }
+}
